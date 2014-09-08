3d189f07eb2a29f3a5fd979682364d0fcf956c7a
samkristoff <samkristoff@openprojectstarter.org>
2014-09-08 13:59:22 -0500

    Added working SendSqsMessage function to private sqs.php Added public SendMessage service.

--- resources/aws/sqs.php
+++ resources/aws/sqs.php
@@ -1,26 +1,32 @@
 <?php
 
-require '../config.php';
+require_once  dirname(__FILE__) . '/../config.php';
 
-$queueUrl = 'https://sqs.us-east-1.amazonaws.com/293388242627/LVH-Revert-Jobs';
-
-$lvVersion = $_GET["version"];
-$fileName = $_GET["fileName"];
+$url = 'https://sqs.us-east-1.amazonaws.com/293388242627/LVH-Revert-Jobs';
 
 use Aws\Sqs\SqsClient;
 
 $AwsSqsClient = SqsClient::factory(array(
-    'key'    => $AwsKey,
-    'secret' => $AwsSecret,
-    'region' => $AwsRegion
+	'key'    => $AwsKey,
+	'secret' => $AwsSecret,
+	'region' => $AwsRegion
 ));
 
-$AwsSqsClient->sendMessage(array(
-											'QueueUrl'    => $queueUrl,
-											'MessageBody' => $lvVersion . "_" . $fileName,
-											)
-								);
- 
- echo 'end';
- 
+//Send an SQS message to the specified queue with the specified message body.  The user must have permission to write to the queue
+function SendSqsMessage($queueUrl, $message)
+{
+	global $AwsSqsClient;
+	$AwsSqsClient->sendMessage(array(
+		'QueueUrl'    => $queueUrl,
+		'MessageBody' => $message
+		));
+}
+
+SendSqsMessage($url, "Test Mes");
+
+function report()
+{
+	echo "Reporting...!";
+}
+
 ?>   
