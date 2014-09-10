e2c6df6a41246a3d2e9130fad76354c5f3687ae8
samkristoff <samkristoff@openprojectstarter.org>
2014-09-10 15:10:22 -0500

    Change JS SQS function names

--- resources/js/aws/sqs.js
+++ resources/js/aws/sqs.js
@@ -7,17 +7,17 @@
 *  See https://github.com/samkristoff/LVH-Fling
 ********************************************************************************************************************/
 
-function SqsSendMessage(queueUrl, message) 
+function SendSqsMessage(queueUrl, message) 
 {
 	HttpGet( SQS_WS_ROOT + "SendMessage.php?queueUrl=" + encodeURIComponent(queueUrl) + "&message=" + encodeURIComponent(message) );
 }
 
-function SqsPeekMessage(queueUrl, searchToken, waitTime) 
+function PeekSqsMessage(queueUrl, searchToken, waitTime) 
 {	
 	return HttpGet( SQS_WS_ROOT  + "PeekMessage.php?queueUrl=" + encodeURIComponent(queueUrl) + "&searchToken=" + encodeURIComponent(searchToken) + "&waitTime=" + encodeURIComponent(waitTime));
 }
 
-function SqsGetMessage(queueUrl, searchToken, waitTime) 
+function GetSqsMessage(queueUrl, searchToken, waitTime) 
 {	
 	return HttpGet( SQS_WS_ROOT + "GetMessage.php?queueUrl=" + encodeURIComponent(queueUrl) + "&searchToken=" + encodeURIComponent(searchToken) + "&waitTime=" + encodeURIComponent(waitTime));	
 }
