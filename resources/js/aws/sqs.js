e08501ad207fdc93bce4162f5e807734d77a3a21
samkristoff <samkristoff@openprojectstarter.org>
2014-09-13 14:30:15 -0500

    Changed GetSqsMessage parameter order to match PHP function Added Create and Delete Sqs Queue functions.  Need to test Delete.

--- resources/js/aws/sqs.js
+++ resources/js/aws/sqs.js
@@ -12,15 +12,33 @@ function SendSqsMessage(queueUrl, message)
 	HttpGet( SQS_WS_ROOT + "SendMessage.php?queueUrl=" + encodeURIComponent(queueUrl) + "&message=" + encodeURIComponent(message) );
 }
 
-function PeekSqsMessage(queueUrl, searchToken, waitTime) 
+function PeekSqsMessage(queueUrl, waitTime, searchToken) 
 {	
 	return HttpGet( SQS_WS_ROOT  + "PeekMessage.php?queueUrl=" + encodeURIComponent(queueUrl) + "&searchToken=" + encodeURIComponent(searchToken) + "&waitTime=" + encodeURIComponent(waitTime));
 }
 
-function GetSqsMessage(queueUrl, searchToken, waitTime) 
+function GetSqsMessage(queueUrl, waitTime, searchToken) 
 {	
-	return HttpGet( SQS_WS_ROOT + "GetMessage.php?queueUrl=" + encodeURIComponent(queueUrl) + "&searchToken=" + encodeURIComponent(searchToken) + "&waitTime=" + encodeURIComponent(waitTime));	
+	return HttpGet( SQS_WS_ROOT + "GetMessage.php?queueUrl=" + encodeURIComponent(queueUrl) + "&waitTime=" + encodeURIComponent(waitTime) + "&searchToken=" + encodeURIComponent(searchToken) );	
 }
 
+//Create An SQS Queue.  Returns The New Queue URL
+function CreateSqsQueue(name, deliveryDelaySec, maxSizeBytes, retentionPeriodSec, recWaitTimeSec, visTimeoutSec) 
+{	
+	
+	var output = HttpGet(SQS_WS_ROOT  + "CreateQueue.php?name=" + encodeURIComponent(name) 
+													+ "&deliveryDelaySec=" + encodeURIComponent(deliveryDelaySec) 
+													+ "&maxSizeBytes=" + encodeURIComponent(maxSizeBytes)
+													+ "&retentionPeriodSec=" + encodeURIComponent(retentionPeriodSec)
+													+ "&recWaitTimeSec=" + encodeURIComponent(recWaitTimeSec)
+													+ "&visTimeoutSec=" + encodeURIComponent(visTimeoutSec)
+							);
+							
+	return output.trim();
+							
+}
 
-
+function DeleteSqsQueue(queueUrl) 
+{	
+	return HttpGet( SQS_WS_ROOT + "DeleteQueue.php?queueUrl=" + encodeURIComponent(queueUrl));
+}
